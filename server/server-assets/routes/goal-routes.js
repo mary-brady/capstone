39eb0b275236caa8877d8a9a6e1e065a8ca2f583
Mary Brady <emceebrady@gmail.com>
2018-09-11 14:49:16 -0600

    goal routes

--- server/server-assets/routes/goal-routes.js
+++ server/server-assets/routes/goal-routes.js
@@ -1,7 +1,66 @@
 let router = require('express').Router()
+let Goals = require('../models/goal')
+
+//GET
+router.get('/', (req, res, next) => {
+    Goals.find({ authorId: req.session.uid })
+        .then(data => {
+            res.send(data)
+        })
+        .catch(err => {
+            res.status(400).send(err)
+            next()
+        })
+})
 
 
+//POST
+router.post('/', (req, res, next) => {
+    req.body.authorId = req.session.uid
+    Goals.create(req.body)
+        .then(newGoal => {
+            res.send(newGoal)
+        })
+        .catch(err => {
+            res.status(400).send(err)
+            next()
+        })
+})
 
+//PUT
+router.put('/:id', (req, res, next) => {
+    Goals.findById(req.params.id)
+        .then(goal => {
+            if (!goal.authorId.equals(req.session.uid)) {
+                return res.status(401).send("Not authorized")
+            }
+            goal.update(req.body, (err) => {
+                if (err) {
+                    console.log(err)
+                    next()
+                    return
+                }
+                res.send("Goal updated")
+            });
+        })
+        .catch(err => {
+            console.log(err)
+            next()
+        })
+})
 
+//DELETE
+router.delete(':/id', (req, res, next) => {
+    Goals.findById(req.params.id)
+        .then(goal => {
+            if (!goal.authorId.equals(req.session.uid)) {
+                return res.status(401).send("Not authorized")
+            }
+            Goals.findByIdAndRemove(req.params.id)
+                .then(data => {
+                    res.send('Delorted!')
+                })
+        })
+})
 
 module.exports = router
