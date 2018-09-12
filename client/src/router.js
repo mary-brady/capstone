c7575fee35869ef00ae4d6b2fe5b571fe12e57bc
Veronica Flores <veronica.marie.flores@gmail.com>
2018-09-12 13:59:46 -0600

    push fixed feed thing

--- client/src/router.js
+++ client/src/router.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Login from './views/Login.vue'
-import Feed from './views/Feed.vue'
+
 import Profile from './views/Profile.vue'
 
 Vue.use(Router)
