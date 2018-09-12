343c2966404fe649f42b356374f544c55041ca2b
charliesands <cwsands3@gmail.com>
2018-09-12 14:02:49 -0600

    working on chart

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,31 +1,55 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <canvas id="planet-chart"></canvas>
+    <router-view />
   </div>
 </template>
 
+<script>
+  import Chart from 'chart.js';
+  import planetChartData from './chart-data.js';
+  export default {
+    name: "app",
+    data() {
+      return {
+        planetChartData: planetChartData,
+      }
+    },
+    mounted() {
+      this.createChart('planet-chart', this.planetChartData);
+    },
+    methods: {
+      createChart(chartId, chartData) {
+        const ctx = document.getElementById(chartId);
+        const myChart = new Chart(ctx, {
+          type: chartData.type,
+          data: chartData.data,
+          options: chartData.options,
+        });
+      }
+    }
+  }
+</script>
+
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+  }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+  #nav {
+    padding: 30px;
+  }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  #nav a {
+    font-weight: bold;
+    color: #2c3e50;
+  }
+
+  #nav a.router-link-exact-active {
+    color: #42b983;
+  }
 </style>
--- client/src/components/GoalDetail.vue
+++ client/src/components/GoalDetail.vue
@@ -1,20 +1,32 @@
 <template>
+  <div id="">
+    <canvas id="planet-chart"></canvas>
 
-
-
-
+  </div>
 </template>
 
 <script>
+  import Chart from 'chart.js';
+  import planetChartData from './chart-data.js';
   export default {
-    name: "goalDetail",
+    name: "app",
     data() {
       return {
-
+        planetChartData: planetChartData,
       }
     },
+    mounted() {
+      this.createChart('planet-chart', this.planetChartData);
+    },
     methods: {
-
+      createChart(chartId, chartData) {
+        const ctx = document.getElementById(chartId);
+        const myChart = new Chart(ctx, {
+          type: chartData.type,
+          data: chartData.data,
+          options: chartData.options,
+        });
+      }
     }
-  };
+  }
 </script>
