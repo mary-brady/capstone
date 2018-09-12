fdff7d82c2523c537f46dae84aa44c656774b0a6
Keith Root <hazelroot11@gmail.com>
2018-09-12 09:54:18 -0600

    Setting up log in/sign up

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,50 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from 'vue';
+import Vuex from 'vuex';
+import Axios from "axios";
+import router from "./router"
 
 Vue.use(Vuex)
 
+let auth = Axios.create({
+  baseURL: "//localhost:3000/auth/",
+  timeout: 3000,
+  withCredentials: true
+})
+
 export default new Vuex.Store({
   state: {
-
+    user: {}
   },
   mutations: {
-
+    setUser(state, user) {
+      state.user = user;
+    }
   },
   actions: {
-
+    //AUTH
+    register({ commit }, newUser) {
+      auth.post("register", newUser).then(res => {
+        commit("setUser", res.data);
+        router.push({ name: "home" });
+      });
+    },
+    authenticate({ commit }) {
+      auth.get("authenticate").then(res => {
+        commit("setUser", res.data);
+        router.push({ name: "home" });
+      });
+    },
+    login({ commit }, creds) {
+      auth.post("login", creds).then(res => {
+        commit("setUser", res.data);
+        router.push({ name: "home" });
+      });
+    },
+    logout({ commit }) {
+      auth.delete("logout").then(() => {
+        commit("clearUser");
+        router.push({ name: "login" });
+      });
+    }
   }
 })
