a4195d60c4625c209910f535086bfb18719d5cd2
Veronica Flores <veronica.marie.flores@gmail.com>
2018-09-12 16:58:37 -0600

    lets you go home now

--- client/src/router.js
+++ client/src/router.js
@@ -11,6 +11,11 @@ Vue.use(Router)
 export default new Router({
   routes: [
     {
+      path: '/',
+      name:'home',
+      component: Home
+    },
+    {
       path: '/login',
       name: 'login',
       // route level code-splitting
