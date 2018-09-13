cb4d7db23b3809b6beb47c4fe4bb684061f3534f
Mary Brady <emceebrady@gmail.com>
2018-09-13 15:59:19 -0600

    modaltown

---
+++ client/src/components/Quest.vue
@@ -0,0 +1,99 @@
+<template>
+<transition name="modal-fade">
+<div class="modal-backdrop">
+<div class="modal">
+    <header class="modal-header">
+        <slot class="header">
+            <h1>DAILY QUEST!</h1>
+        </slot>
+        </header>
+        <div class="modal-body">
+        <slot class="body">
+            <div class="mw">
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis et eos corporis cupiditate, inventore libero illo, ratione perferendis debitis architecto aperiam aspernatur repellat blanditiis. Cumque ab, amet esse quis sit modi fuga quam in, ullam ipsam officiis velit autem accusamus recusandae sequi vero numquam dicta repudiandae ipsa a. Repudiandae, eos.</p>
+            </div>
+        </slot>
+        </div>
+        <div class="modal-footer">
+        <slot class="footer">
+            <p>Do you accept?</p>
+            <button type="button" class="btn btn-primary" @click="close">
+                Yep
+                </button>
+            <button type="button" class="btn btn-secondary" @click="close">
+                Nope
+                </button>
+        </slot>
+        </div>
+    </div>
+
+</div>
+</transition>
+</template>
+<script>
+export default {
+  name: "modal",
+
+  methods: {
+    close() {
+      this.$emit("close");
+    }
+  }
+};
+</script>
+
+<style scoped>
+.modal-backdrop {
+  position: fixed;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  background-color: rgba(0, 0, 0, 0.3);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.modal {
+  position: relative;
+  background: #ffffff;
+  box-shadow: 2px 2px 20px 1px;
+  overflow-x: auto;
+  display: flex;
+  flex-direction: column;
+  max-width: 40%;
+}
+
+.modal-header,
+.modal-footer {
+  padding: 15px;
+  display: flex;
+}
+
+.modal-header {
+  border-bottom: 1px solid #78c2ad;
+  color: #4aae9b;
+  justify-content: center;
+}
+
+.modal-footer {
+  border-top: 1px solid #78c2ad;
+  justify-content: flex-end;
+}
+
+.modal-body {
+  position: relative;
+  padding: 20px 10px;
+}
+
+.modal-fade-enter,
+.modal-fade-leave-active {
+  opacity: 0;
+}
+
+.modal-fade-enter-active,
+.modal-fade-leave-active {
+  transition: opacity 0.75s ease;
+}
+</style>
