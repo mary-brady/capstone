00846f70fe86460b547b5df54325ad99c9f1d0db
Mary Brady <emceebrady@gmail.com>
2018-09-16 15:23:04 -0600

    burg menu semi working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,36 +1,101 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/login">Login</router-link> |
-      <router-link to="/profile">Profile</router-link> |
+      <Push>
+        <router-link to="/">Home</router-link>  
+      <router-link to="/login">Login</router-link>
+      <router-link to="/profile">Profile</router-link>
       <router-link to="/goal">Goals</router-link>
+      </Push>
     </div>
     <router-view />
   </div>
 </template>
+<script>
+import { Push } from "vue-burger-menu";
+export default {
+  name: "App",
+  components: {
+    Push
+  }
+};
+</script>
 
 
 
 <style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-  #nav {
-    padding: 30px;
-  }
+#nav a {
+  font-weight: bold;
+  color: rgb(160, 222, 231);
+}
 
-  #nav a {
-    font-weight: bold;
-    color: #2c3e50;
-  }
+.bm-burger-button {
+  position: fixed;
+  width: 36px;
+  height: 30px;
+  left: 36px;
+  top: 36px;
+  cursor: pointer;
+}
+.bm-burger-bars {
+  background-color: #373a47;
+}
+.line-style {
+  position: absolute;
+  height: 20%;
+  left: 0;
+  right: 0;
+}
+.cross-style {
+  position: absolute;
+  top: 12px;
+  right: 2px;
+  cursor: pointer;
+}
+.bm-cross {
+  background: #bdc3c7;
+}
+.bm-cross-button {
+  height: 24px;
+  width: 24px;
+}
+.bm-menu {
+  height: 100%;
+  width: 0;
+  position: fixed;
+  z-index: 1000;
+  top: 0;
+  left: 0;
+  background-color: rgb(63, 63, 65);
+  overflow-x: hidden;
+  padding-top: 60px;
+  transition: 0.5s;
+}
 
-  #nav a.router-link-exact-active {
-    color: #42b983;
-  }
+.bm-overlay {
+  background: rgba(0, 0, 0, 0.3);
+}
+.bm-item-list {
+  color: #b8b7ad;
+  margin-left: 10%;
+  font-size: 20px;
+}
+.bm-item-list > * {
+  display: flex;
+  text-decoration: none;
+  padding: 0.7em;
+}
+.bm-item-list > * > span {
+  margin-left: 10px;
+  font-weight: 700;
+  color: white;
+}
 </style>
