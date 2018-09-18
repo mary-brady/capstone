7af632ea0c848fbc643d3afcf166ba7fca694409
Tavia Sterling <TaviaSterling@outlook.com>
2018-09-18 11:20:00 -0600

    fix quote route

--- server/index.js
+++ server/index.js
@@ -72,7 +72,7 @@ let strengthRoutes = require('./server-assets/routes/strength-route')
 server.use('/api/strength', strengthRoutes)
 
 let quoteRoutes = require('./server-assets/routes/quote-routes')
-server.use('api/quotes', quoteRoutes)
+server.use('/api/quotes', quoteRoutes)
 
 //Catch all
 server.get('*', (req, res, next) => {
