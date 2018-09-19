4c5c2f61e0888f6df18de001c72cecea104f705b
Mary Brady <emceebrady@gmail.com>
2018-09-19 10:37:35 -0600

    workout changes/tests

--- client/src/views/Workouts.vue
+++ client/src/views/Workouts.vue
@@ -1,7 +1,22 @@
 <template>
-    <div id="workouts">
-      <h1>Hi this is workouts!</h1>
-
+    <div id="workouts" class="container-fluid">
+      <h1>WELCOME TO WORKOUT TOWN</h1>
+      <div class="card border-primary mb-3" style="max-width: 20rem">
+        <div class="card-header">
+          Workouts
+        </div>
+        <h4 class="card-title">Workout Title Here</h4>
+        <p class="card-text">
+          Blahblahblah
+        </p>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Test</li>
+          <li class="list-group-item">Testing</li>
+          <li class="list-group-item">1 2 3</li>
+        </ul>
+      </div>
+      <button class="btn btn-primary">Strength Exercises</button>
+      <button class="btn btn-secondary">Cardio Exercises</button>
     </div>
 </template>
 <script>
